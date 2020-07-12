dc5bab425bc316ae60c2576d3e751593201983d2
Edinson Aldaz <eduar2083@gmail.com>
2020-07-11 20:18:15 -0500

    Modificación archivo sql

--- BD/bd_insn_version2.sql
+++ BD/bd_insn_version2.sql
@@ -2,30 +2,13 @@ DROP DATABASE IF EXISTS bd_insn;
 
 CREATE DATABASE `bd_insn` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bd_insn`;
--- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: bd_insn
--- ------------------------------------------------------
--- Server version	8.0.18
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE `cargo` (
+  `codCargo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  constraint pk_cargo PRIMARY KEY (`codCargo`)
+);
 
---
--- Table structure for table `bien`
---
-
-DROP TABLE IF EXISTS `bien`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bien` (
   `codBien` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(90) NOT NULL,
@@ -33,111 +16,8 @@ CREATE TABLE `bien` (
   `tipo` int(11) NOT NULL,
   `fechaRegistro` date NOT NULL,
   PRIMARY KEY (`codBien`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Dumping data for table `bien`
---
+);
 
-LOCK TABLES `bien` WRITE;
-/*!40000 ALTER TABLE `bien` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bien` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cargo`
---
-
-DROP TABLE IF EXISTS `cargo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cargo` (
-  `codCargo` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codCargo`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cargo`
---
--- Table structure for table `cargo_enlace`
---
-
-DROP TABLE IF EXISTS `cargo_enlace`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cargo_enlace` (
-  `codCargo` int(11) NOT NULL,
-  `codEnlace` int(11) NOT NULL,
-  PRIMARY KEY (`codCargo`,`codEnlace`),
-  KEY `fk_CARGO_has_ENLACE_ENLACE1_idx` (`codCargo`),
-  KEY `fk_CARGO_has_ENLACE_CARGO1_idx` (`codEnlace`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cargo_enlace`
---
-
-LOCK TABLES `cargo_enlace` WRITE;
-/*!40000 ALTER TABLE `cargo_enlace` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cargo_enlace` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `enlace`
---
-
-DROP TABLE IF EXISTS `enlace`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `enlace` (
-  `codEnlace` int(11) NOT NULL,
-  `descripcion` varchar(45) DEFAULT NULL,
-  `ruta` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codEnlace`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `enlace`
---
-
---
--- Table structure for table `unidadorganica`
---
-
-DROP TABLE IF EXISTS `unidadorganica`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `unidadorganica` (
-  `codUnidad` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(90) NOT NULL,
-  `descripcion` varchar(500) NOT NULL,
-  `anexo` varchar(5) NOT NULL,
-  `jefeEncargado` int(11) NOT NULL,
-  `fechaRegistro` date NOT NULL,
-  PRIMARY KEY (`codUnidad`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `unidadorganica`
---
-
-LOCK TABLES `unidadorganica` WRITE;
-/*!40000 ALTER TABLE `unidadorganica` DISABLE KEYS */;
-/*!40000 ALTER TABLE `unidadorganica` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `codUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `nroDocumento` varchar(8) NOT NULL,
@@ -147,33 +27,50 @@ CREATE TABLE `usuario` (
   `clave` varchar(15) NOT NULL,
   `correo` varchar(30) NOT NULL,
   `fechaRegistro` date NOT NULL,
-  `idCargo` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `codCargo` int(11) DEFAULT NULL,
+  
+  constraint pk_usuario PRIMARY KEY (`codUsuario`),
+  constraint fk_usuario_cargo foreign key(codCargo) references cargo(codCargo)
+);
 
---
--- Dumping data for table `usuario`
+CREATE TABLE `unidadorganica` (
+  `codUnidad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(90) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `anexo` varchar(5) NOT NULL,
+  `jefeEncargado` int(11) NOT NULL,
+  `fechaRegistro` date NOT NULL,
+  CONSTRAINT pk_unidadorganica PRIMARY KEY (`codUnidad`),
+  CONSTRAINT fk_unidadorganica_usuario foreign key(jefeEncargado) references usuario(codUsuario)
+);
 
---
--- Dumping routines for database 'bd_insn'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+DELIMITER $$
+CREATE UNIQUE INDEX id_cargo_nombre ON cargo(nombre)
+$$
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE UNIQUE INDEX idx_usuario_nroDocumento ON usuario(nroDocumento)
+$$
 
-ALTER TABLE usuario
-CHANGE COLUMN fechaRegistro fechaRegistro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE bien
-CHANGE COLUMN fechaRegistro fechaRegistro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE unidadorganica
-CHANGE COLUMN fechaRegistro fechaRegistro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+CREATE UNIQUE INDEX idx_bien_nombre ON bien(nombre)
+$$
 
+CREATE TRIGGER `tgr_usuario_binsert` BEFORE INSERT ON `usuario`
+FOR EACH ROW BEGIN
+        -- Set the udpate date
+    Set new.fechaRegistro = now();
+END
+$$
 
--- Dump completed on 2020-07-10 13:24:29
+CREATE TRIGGER `tgr_bien_binsert` BEFORE INSERT ON `bien`
+FOR EACH ROW BEGIN
+        -- Set the udpate date
+    Set new.fechaRegistro = now();
+END
+$$
+
+CREATE TRIGGER `tgr_unidadOrganica_binsert` BEFORE INSERT ON `unidadOrganica`
+FOR EACH ROW BEGIN
+        -- Set the udpate date
+    Set new.fechaRegistro = now();
+END
+$$
