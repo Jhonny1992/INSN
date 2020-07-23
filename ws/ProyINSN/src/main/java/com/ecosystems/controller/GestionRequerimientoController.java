ae88ff297873097724911f8c6fd699564fed8266
Crios23 <rioscris.291197@gmail.com>
2020-07-22 23:42:56 -0500

    vacios

--- ws/ProyINSN/src/main/java/com/ecosystems/controller/GestionRequerimientoController.java
+++ ws/ProyINSN/src/main/java/com/ecosystems/controller/GestionRequerimientoController.java
@@ -1,16 +1,26 @@
 package com.ecosystems.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ecosystems.entity.BienBean;
+import com.ecosystems.services.BienService;
 
 @Controller
 @RequestMapping(value = "/gestion")
 
 public class GestionRequerimientoController {
-
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "gestionrequerimiento";
 	}
+	
+	
 }
